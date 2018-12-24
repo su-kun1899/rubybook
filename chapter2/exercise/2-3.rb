0e8a1c97a9df7cd8ddd4a772232ba789dd8f2c78
su-kun1899 <higedrum.coz@gmail.com>
2018-12-24 20:14:38 +0900

    CHAPTER2 練習問題

---
+++ chapter2/exercise/2-3.rb
@@ -0,0 +1,23 @@
+# 問6
+coffee_label = "コーヒー"
+coffee_price = 300
+puts "#{coffee_label}: #{coffee_price}円"
+
+espresso_label = "エスプレッソ"
+espresso_price = 100
+puts "#{espresso_label}: #{espresso_price}円"
+puts "#{espresso_label}: #{espresso_price}円"
+
+puts "合計: #{coffee_price + (espresso_price * 2)}円"
+
+# 問7
+coffee_label = "コーヒー"
+coffee_price = 400
+puts "#{coffee_label}: #{coffee_price}円"
+
+espresso_label = "エスプレッソ"
+espresso_price = 100
+puts "#{espresso_label}: #{espresso_price}円"
+puts "#{espresso_label}: #{espresso_price}円"
+
+puts "合計: #{coffee_price + (espresso_price * 2)}円"
