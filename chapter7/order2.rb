3583a11b139b77ef65d0f60c1c9da8f8dd579533
su-kun1899 <higedrum.coz@gmail.com>
2019-05-01 22:06:38 +0900

    引数の便利な機能を使う

---
+++ chapter7/order2.rb
@@ -0,0 +1,7 @@
+def order2(item = "コーヒー")
+  "#{item}をください"
+end
+
+puts order2
+puts order2 "カフェラテ"
+puts order2 "モカ"
---
+++ chapter7/order4.rb
@@ -0,0 +1,6 @@
+def order4(item:, size:)
+  "#{item}を#{size}サイズでください"
+end
+
+puts order4(item: "カフェラテ", size: "ベンティ")
+puts order4(size: "ベンティ", item: "カフェラテ")
